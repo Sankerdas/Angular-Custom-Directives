55fe1549758d0d0bab471e02da9504b66c6bba9f
Sanker das <sankerdas.yes@gmail.com>
2020-01-10 10:16:33 -0800

    creating custom directive
    An angular attribute directive can be simply described as a component without a template
    
    ng generate directive directives/example
    
    it will create directive named exampleDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleDirective } from './directives/example.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleDirective
   ],
   imports: [
     BrowserModule,
